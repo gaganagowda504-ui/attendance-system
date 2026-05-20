cdb778d9c04ef6a4701a1d127b4c00d6502da47f
gagana <gaganagowda504@gmail.com>
2026-05-20 22:58:10 +0530

    Updated attendance monitoring system

--- backend/app.py
+++ backend/app.py
@@ -1,34 +1,53 @@
-from flask import Flask, jsonify
+from flask import Flask, request
 from flask_cors import CORS
-import csv
+import cv2
+import numpy as np
+import base64
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades +
+    'haarcascade_frontalface_default.xml'
+)
+@app.route('/')
 def home():
     return "Backend Running Successfully"
+@app.route('/upload', methods=['POST'])
+def upload():
 
-@app.route("/attendance")
-def attendance():
-    data = []
+    data = request.json
 
-    try:
-        with open("attendance.csv", "r") as file:
-            reader = csv.reader(file)
-            next(reader)
+    name = data['name']
 
-            for row in reader:
-                if len(row) == 3:
-                    data.append({
-                        "name": row[0],
-                        "date": row[1],
-                        "time": row[2]
-                    })
-    except:
-        return jsonify([])
+    image_data = data['image'].split(",")[1]
 
-    return jsonify(data)
+    img_bytes = base64.b64decode(image_data)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    np_arr = np.frombuffer(img_bytes, np.uint8)
+
+    img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(
+        gray,
+        1.1,
+        4
+    )
+
+    if len(faces) > 0:
+
+        return {
+            "message": f"{name} Attendance Marked"
+        }
+
+    else:
+
+        return {
+            "message": "No Face Detected"
+        }
+
+if __name__ == '__main__':
+    app.run(debug=True) 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,115 @@
+import { CSVLink } from "react-csv";
+import "./App.css";
+import Webcam from "react-webcam";
+import { useRef, useState } from "react";
+import axios from "axios";
+
 function App() {
 
-  const attendanceData = [
-    {
-      name: "Gagana",
-      date: "19-05-2026",
-      time: "10:00 AM"
-    },
-    {
-      name: "Rahul",
-      date: "19-05-2026",
-      time: "10:05 AM"
+  const webcamRef = useRef(null);
+
+  const [name, setName] = useState("");
+
+  const [attendanceData, setAttendanceData] = useState(
+    JSON.parse(localStorage.getItem("attendance")) || []
+  );
+
+  const capture = async () => {
+
+    const imageSrc = webcamRef.current.getScreenshot();
+
+    const response = await axios.post(
+      "http://127.0.0.1:5000/upload",
+      {
+        image: imageSrc,
+        name: name
+      }
+    );
+
+    const currentDate = new Date().toLocaleDateString();
+
+   const currentTime = new Date().toLocaleTimeString([], {
+  hour: '2-digit',
+  minute: '2-digit'
+});
+
+    if (response.data.message.includes("Attendance Marked")) {
+
+      const newAttendance = {
+        name: name,
+        date: currentDate,
+        time: currentTime
+      };
+
+      const updatedAttendance = [
+        ...attendanceData,
+        newAttendance
+      ];
+
+      setAttendanceData(updatedAttendance);
+
+      localStorage.setItem(
+        "attendance",
+        JSON.stringify(updatedAttendance)
+      );
     }
-  ];
+
+    alert(response.data.message);
+
+    setName("");
+  };
 
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Attendance System</h1>
+    <div className="container">
 
+      <h1>Attendance Monitoring System</h1>
+
+      {/* INPUT */}
+      <input
+        type="text"
+        placeholder="Enter Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        style={{
+          padding: "10px",
+          fontSize: "18px"
+        }}
+      />
+
+      <br /><br />
+
+      {/* WEBCAM */}
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={400}
+      />
+
+      <br /><br />
+
+      {/* BUTTON */}
+      <button
+        className="button"
+        onClick={capture}
+        style={{
+          padding: "10px",
+          fontSize: "18px"
+        }}
+      >
+        Mark Attendance
+      </button>
+
+      <br /><br />
+
+      {/* TABLE */}
       <table
+        className="table"
         border="1"
-        cellPadding="12"
+        cellPadding="10"
         style={{
           borderCollapse: "collapse",
-          fontSize: "24px"
+          fontSize: "18px"
         }}
       >
         <thead>
@@ -42,7 +129,23 @@ function App() {
             </tr>
           ))}
         </tbody>
+
       </table>
+
+      <br />
+
+      {/* DOWNLOAD CSV */}
+      <CSVLink
+        data={attendanceData}
+        filename={"attendance.csv"}
+      >
+
+        <button className="button">
+          Download Attendance CSV
+        </button>
+
+      </CSVLink>
+
     </div>
   );
 }
